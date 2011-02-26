36262d066ee527434422d744ebb73bd4409972c9
Jan Kuchař <honza@mujserver.net>
2011-02-26 03:16:31 +0000

    - doplněn zbytek

---
+++ libs/PdfResponse/mpdf/ttfontdata/dejavuserifcondensedBI.mtx.php
@@ -0,0 +1,24 @@
+<?php
+$name='DejaVuSerifCondensedBoldItalic';
+$type='TTF';
+$desc=array (
+  'Ascent' => 939,
+  'Descent' => -236,
+  'CapHeight' => 939,
+  'Flags' => 262212,
+  'FontBBox' => '[-815 -389 1579 1235]',
+  'ItalicAngle' => -11,
+  'StemV' => 165,
+  'MissingWidth' => 540,
+);
+$up=-63;
+$ut=44;
+$ttffile='D:\www\Apache\01Vyvoj\GrifartIS\head\source\PHP\document_root/../libs/PdfResponse/mpdf/ttfonts/DejaVuSerifCondensed-BoldItalic.ttf';
+$TTCfontID='0';
+$originalsize=294584;
+$sip=false;
+$smp=false;
+$BMPselected=true;
+$fontkey='dejavuserifcondensedBI';
+$panose='0 0 2 6 8 6 5 3 5 11 2 4';
+?>
